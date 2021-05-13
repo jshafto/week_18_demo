1ff69e6c66653a278fa3126c70499cf7834dce3d
jshafto <jpshafto@gmail.com>
2021-05-13 10:34:57 -0400

    before migrations

--- database.py
+++ database.py
@@ -6,42 +6,42 @@ from app.models import Widget, Store  # noqa
 
 with app.app_context():
     db.drop_all()
-    db.create_all()
+    # db.create_all()
 
-    new_widget = Widget(color="red")
-    two_widget = Widget(color="blue")
-    three_widget = Widget(color="rpurple")
-    four_widget = Widget(color="rorange")
-    five_widget = Widget(color="chartreuse")
-    db.session.add(new_widget)
-    db.session.add(two_widget)
-    db.session.add(three_widget)
-    db.session.add(four_widget)
-    db.session.add(five_widget)
+    # new_widget = Widget(color="red")
+    # two_widget = Widget(color="blue")
+    # three_widget = Widget(color="rpurple")
+    # four_widget = Widget(color="rorange")
+    # five_widget = Widget(color="chartreuse")
+    # db.session.add(new_widget)
+    # db.session.add(two_widget)
+    # db.session.add(three_widget)
+    # db.session.add(four_widget)
+    # db.session.add(five_widget)
 
-    store_one = Store(name="Marni's Widget Shoppe")
-    store_two = Store(name="Ed's Wholesale")
+    # store_one = Store(name="Marni's Widget Shoppe")
+    # store_two = Store(name="Ed's Wholesale")
 
-    db.session.add(store_one)
-    db.session.add(store_two)
+    # db.session.add(store_one)
+    # db.session.add(store_two)
 
-    print(new_widget.stores, "-------")
-    new_widget.stores.append(store_one)
-    print(new_widget.stores, "-------")
+    # print(new_widget.stores, "-------")
+    # new_widget.stores.append(store_one)
+    # print(new_widget.stores, "-------")
 
-    store_two.widgets.append(two_widget)
-    print(store_two.widgets, "-------")
+    # store_two.widgets.append(two_widget)
+    # print(store_two.widgets, "-------")
 
-    store_two.widgets = [four_widget, five_widget]
-    print(store_two.widgets, "-------")
+    # store_two.widgets = [four_widget, five_widget]
+    # print(store_two.widgets, "-------")
 
-    db.session.commit()
-    print(store_two.widgets, "-------")
+    # db.session.commit()
+    # print(store_two.widgets, "-------")
 
-    store_two.widgets.remove(four_widget)
-    db.session.add(store_two)
-    db.session.commit()
-    print(store_two.widgets, "-------")
+    # store_two.widgets.remove(four_widget)
+    # db.session.add(store_two)
+    # db.session.commit()
+    # print(store_two.widgets, "-------")
 
     # widgets = Widget.query.filter(Widget.color == "blue").all()
     # print([widget.color for widget in widgets])
